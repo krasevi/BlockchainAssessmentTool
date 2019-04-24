00319cbea5c5fabed01e802fb85bbb79fe238dd5
krasevi <krasevi@github.com>
2019-04-25 01:14:38 +0200

    added stats for additions, deletions, changes per repository

--- BlockchainAssessmentTool.py
+++ BlockchainAssessmentTool.py
@@ -1,9 +1,10 @@
 from github import Github
 
-# using username and password
+
 
 # Attention always delete password before committing
 # ------------------------------------------------------------------------------
+# using username and password
 g = Github("krasevi", "xxx")
 # or using an access token
 # g = Github("access_token")
@@ -16,11 +17,14 @@ g = Github("krasevi", "xxx")
     # print(repo.name)
     # repo.edit(has_wiki=False)
     # to see all the available attributes and methods
-    # print(dir(repo))
+    # print(dir(repo)
+
+# ******************************************************************************
+# 1. Statistics per repository
+# ******************************************************************************
 
 # define repository
-repo = g.get_repo("krasevi/BlockchainAssessmentTool")
-
+repo = g.get_repo("openshift/installer")
 
 # stars
 starCount = repo.stargazers_count
@@ -44,14 +48,25 @@ print(forkCount)
 # stats
 stats = repo.get_stats_code_frequency()
 code_frequency = list(stats)
-additions = code_frequency[-1].additions
-deletions = code_frequency[-1].deletions
+
+# stats last week
 week = code_frequency[-1].week
-print (additions, deletions, week)
+additions_week = int(code_frequency[-1].additions)
+deletions_week = int(code_frequency[-1].deletions)
+changes_week = abs(additions_week)+abs(deletions_week)
+print(week, additions_week, deletions_week, changes_week)
+
+# stats last month
+month = [-1, -2, -3, -4]
+additions_month = sum(int(code_frequency[i].additions) for i in month)
+deletions_month = sum(int(code_frequency[i].deletions) for i in month)
+changes_month = abs(additions_month) + abs(deletions_month)
+print(additions_month, deletions_month, changes_month)
 
 # number of contributors
 contributors = repo.get_contributors()
 contributor_list = []
 for contributor in contributors:
     contributors = contributor_list.append(contributor.name)
-print(len(contributor_list))
+contributorCount = len(contributor_list)
+print(contributorCount)
