249ade569c9347f97a8438743983b28f25ade441
krasevi <krasevi@github.com>
2019-04-26 16:10:48 +0200

    added statistics for commit activity

--- BlockchainAssessmentTool.py
+++ BlockchainAssessmentTool.py
@@ -1,62 +1,89 @@
+# author: Severin Kranz
+# GitHub: https://github.com/krasevi
+
+# ******************************************************************************
+# prerequisites
+# ******************************************************************************
+# 1. install pyGithub by using "pip install pyGithub" in the command line
+
 from github import Github
 
+# ******************************************************************************
+# settings
+# ******************************************************************************
 
-
-# Attention always delete password before committing
-# ------------------------------------------------------------------------------
 # using username and password
+# Attention always delete password before committing to GitHub
 g = Github("krasevi", "xxx")
 # or using an access token
 # g = Github("access_token")
 # Github Enterprise with custom hostname
 # g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
-# ------------------------------------------------------------------------------
-
-# get repository names of chosen user
-# for repo in g.get_user().get_repos():
-    # print(repo.name)
-    # repo.edit(has_wiki=False)
-    # to see all the available attributes and methods
-    # print(dir(repo)
+# repository
+repositoryName = "NebulousLabs/Sia"
+# active developer definition
+activeDevCommits = 50
+activeDevChanges = 100
 
 # ******************************************************************************
 # 1. Statistics per repository
 # ******************************************************************************
 
 # define repository
-repo = g.get_repo("openshift/installer")
+repo = g.get_repo(repositoryName)
 
 # stars
-starCount = repo.stargazers_count
-print(starCount)
-
-# commits
-commits = repo.get_commits()
-commitCount = commits.totalCount
-print(commitCount)
+stars_totalCount = repo.stargazers_count
+print(stars_totalCount)
 
 # events
 events = repo.get_events()
-eventCount = events.totalCount
-print(eventCount)
+events_totalCount = events.totalCount
+print(events_totalCount)
 
 # forks
 forks = repo.get_forks()
-forkCount = forks.totalCount
-print(forkCount)
+forks_totalCount = forks.totalCount
+print(forks_totalCount)
 
-# stats
-stats = repo.get_stats_code_frequency()
-code_frequency = list(stats)
+# commits
+commits = repo.get_commits()
+commit_totalCount = commits.totalCount
+print(commit_totalCount)
 
-# stats last week
+# commit_activity
+# Get the last year of commit activity data grouped by week
+# probably only commits by owners are counted - tbd
+commit_activity_stats = repo.get_stats_commit_activity()
+print(list(commit_activity_stats))
+commit_activity_list_week = []
+commit_activity_list_days = []
+for StatsCommitActivity in commit_activity_stats:
+    commit_activity_stats = commit_activity_list_week.append(StatsCommitActivity.total)
+    commit_activity_stats = commit_activity_list_days.append(StatsCommitActivity.days)
+
+# commit_activity last week
+print(commit_activity_list_week[-1])
+print(commit_activity_list_days[-1])
+
+# commit_activity last changes_month
+month = [-1, -2, -3, -4]
+commit_activity_month = sum(int(commit_activity_list_week[i]) for i in month)
+print(commit_activity_month)
+
+# code_frequency
+# Get the number of additions and deletions per week
+# probably only gets code frequency of owners - tbd
+code_frequency = list(repo.get_stats_code_frequency())
+
+# code_frequency in the last week
 week = code_frequency[-1].week
 additions_week = int(code_frequency[-1].additions)
 deletions_week = int(code_frequency[-1].deletions)
 changes_week = abs(additions_week)+abs(deletions_week)
 print(week, additions_week, deletions_week, changes_week)
 
-# stats last month
+#  code_frequency in the last month
 month = [-1, -2, -3, -4]
 additions_month = sum(int(code_frequency[i].additions) for i in month)
 deletions_month = sum(int(code_frequency[i].deletions) for i in month)
@@ -68,5 +95,30 @@ contributors = repo.get_contributors()
 contributor_list = []
 for contributor in contributors:
     contributors = contributor_list.append(contributor.name)
-contributorCount = len(contributor_list)
-print(contributorCount)
+contributor_totalCount = len(contributor_list)
+print(contributor_totalCount)
+
+# number of pull requests
+pulls = repo.get_pulls()
+pulls_numbers_list = []
+for pull in pulls:
+    pulls_numbers_list.append(pull.number)
+pulls_totalCount = len(pulls_numbers_list)
+print(pulls_totalCount)
+print(pulls)
+
+# Get participation stats for a repo
+# = Get the weekly commit count for the repository owner and everyone else
+participation_stats = repo.get_stats_participation()
+
+# ******************************************************************************
+# 2. statistics for active developers
+# ******************************************************************************
+
+# define active developers per repository
+
+# Get commits to a repo
+commits = repo.get_commits()
+commits_sha_list = []
+for commit in commits:
+    commits_sha_list.append(commit.sha)
